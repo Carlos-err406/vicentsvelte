04975954cefa54f81395342eb3ee71fe2655b163
Carlos-err406 <carlosvilaseca406@gmail.com>
2022-09-09 17:24:22 -0400

    initial commit

---
+++ src/components/View.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { counter } from '../store';
+	import Button from './Button.svelte';
+    const up = () => {
+        $counter++;
+    };
+    const down = () => {
+        $counter--;
+    }
+</script>
+
+<h1>{$counter}</h1>
+<Button value={'+'} on:click={up}/>
+<Button value={'-'} on:click={down}/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+   import View from '../components/View.svelte'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<View/>
